33e11a605d4c17c2cec71c53bcf80e74dfcc48ac
gadimbaylisahil <sahil.gadimbay@gmail.com>
2018-08-20 15:09:45 +0200

    Fetch bicycle and options and variants on ShowBicycle container

--- src/containers/Customer/Shop/Bicycle/ShowBicycle.js
+++ src/containers/Customer/Shop/Bicycle/ShowBicycle.js
@@ -6,26 +6,35 @@ import * as actions from "../../../../store/actions";
 class ShowBicycle extends Component {
   componentDidMount(){
     this.props.getBicycle(this.props.match.params.id)
+        .then( response => {
+          this.getOptions(this.props.match.params.id)
+          this.getVariants(this.props.match.params.id)
+        })
+        .catch( error => {
+          console.log('Bicycle not found')
+        })
   }
   
   render(){
     return(
         <div>
-        
+          Hi there
         </div>
     )
   }
 }
 
-const mapStateToProps = (dispatch) => {
+const mapStateToProps = (state) => {
   return {
-    bicycle: state => state.bicycle.bicycle
+    bicycle: state => state.bicycle.bicycle,
+    options: state => state.bicycle.options
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    getBicycle: (id) => dispatch(actions.fetchBicycle(id))
+    getBicycle: (id) => dispatch(actions.fetchBicycle(id)),
+    getOptions: (id) => dispatch(actions.fetchOptions(id))
   }
 }
 
